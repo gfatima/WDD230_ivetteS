f8a63828b2d420dd6dbc31d811e0aa1f9de8d861
gfatima <37168871+gfatima@users.noreply.github.com>
2021-05-08 22:51:15 -0500

    Version 1.2

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
-
+// Last Update
+var date = document.lastModified;
+document.getElementById("lastUpdate").textContent = " " + date;
+// Year
+const year = new Date();
+document.getElementById("copyYear").textContent = year.getFullYear();
 
