5905ba22d4057080d01cc3a95d8f3402aaf37db7
gfatima <37168871+gfatima@users.noreply.github.com>
2021-05-15 20:25:24 -0500

    Version 1.4

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-// Last Update
-var date = document.lastModified;
-document.getElementById("lastUpdate").textContent = " " + date;
+const d = new Date(document.lastModified);
+const dayName = d.getDay();
+const monthName = d.getMonth();
+const year = d.getFullYear();
+//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById("lastUpdate").textContent = " " + d;
 // Year
-const year = new Date();
 document.getElementById("copyYear").textContent = year.getFullYear();
-
---
+++ lesson4/js/script.js
@@ -0,0 +1,3 @@
+function toggleMenu() {
+  document.getElementById("despl-nav").classList.toggle("hide");
+}
