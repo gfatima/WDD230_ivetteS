1460c3b5e2404db394c11341fa8de43aa66c313a
gfatima <37168871+gfatima@users.noreply.github.com>
2021-05-28 16:31:02 -0500

    version 1.55

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
-const d = new Date(document.lastModified);
-const dayName = d.getDay();
-const monthName = d.getMonth();
-const year = d.getFullYear();
+//const d = new Date(document.lastModified);
+
+const d = new Date();
+
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+let months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+let dayOfWeek = days[d.getDay()];
+let month = months[d.getMonth()];
+let day = d.getUTCDate();
+let year = d.getFullYear();
+
 //const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-document.getElementById("lastUpdate").textContent = " " + d;
+const fulldate = `${dayOfWeek}, ${day} ${month} ${year}`;
+document.getElementById("lastUpdate").textContent = " " + fulldate;
 // Year
-document.getElementById("copyYear").textContent = year.getFullYear();
+document.getElementById("copyYear").textContent = d.getFullYear();
