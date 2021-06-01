b437d70887d7ab4bbd8933bfaa85ca73fd59fccf
gfatima <37168871+gfatima@users.noreply.github.com>
2021-06-01 00:21:01 -0500

    version 1.6

--- js/index.js
+++ js/index.js
@@ -20,12 +20,15 @@ let months = [
 ];
 
 let dayOfWeek = days[d.getDay()];
-let month = months[d.getMonth()];
+let month = months[d.getMonth()+1];
 let day = d.getUTCDate();
 let year = d.getFullYear();
+let hour = d.getHours();
+let min = d.getMinutes();
+let second = d.getSeconds();
 
 //const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayOfWeek}, ${day} ${month} ${year}`;
+const fulldate = `${dayOfWeek}, ${day} ${month} ${year}, ${hour}:${min}:${second}`;
 document.getElementById("lastUpdate").textContent = " " + fulldate;
 // Year
 document.getElementById("copyYear").textContent = d.getFullYear();
