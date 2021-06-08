1f254e1a40ed6e39b420c362749a3a71858bd454
gfatima <37168871+gfatima@users.noreply.github.com>
2021-06-07 20:02:14 -0500

    version 1.82

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -1,3 +1,7 @@
 function toggleMenu() {
   document.getElementById("despl-nav").classList.toggle("hide");
 }
+
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
