46262d5c5203d5637f9f2281bb4c7cf0e290767f
gfatima <37168871+gfatima@users.noreply.github.com>
2021-06-27 12:08:13 -0500

    version 10.1

--- lesson10/js/weatherForecast.js
+++ lesson10/js/weatherForecast.js
@@ -1,5 +1,5 @@
 const cityid = "5604473";
-//const APPID = `b0642f48af45c4e3841e545ddb50089c`
+const APPID = `b0642f48af45c4e3841e545ddb50089c`
 
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
 
