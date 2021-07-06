c8a0c5b8736a36ec886d4b9c52c9245a896f2c37
gfatima <37168871+gfatima@users.noreply.github.com>
2021-07-06 13:41:27 -0500

    version 1.11b

--- lesson11/js/sodaweatherForecast.js
+++ lesson11/js/sodaweatherForecast.js
@@ -1,4 +1,4 @@
-const cityid = "5604473";
+const cityid = "5607916";
 const APPID = `b0642f48af45c4e3841e545ddb50089c`
 
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
